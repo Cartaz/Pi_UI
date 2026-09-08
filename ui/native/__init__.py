2f3c35ea18b16e0eb1701d0019fe90f859612a10
Cartaz <prenc.francesco@gmail.com>
2026-09-08 20:28:17 +0200

    feat: export app shutdown coordinator

--- ui/native/__init__.py
+++ ui/native/__init__.py
@@ -1,9 +1,11 @@
 """Native Qt integrations owned by Pi_UI."""
 
 from .agent_process import QProcessAgentTransport, QProcessTransportError
+from .app_shutdown import AppShutdownCoordinator
 from .deadline_scheduler import QtDeadlineScheduler
 
 __all__ = [
+    "AppShutdownCoordinator",
     "QProcessAgentTransport",
     "QProcessTransportError",
     "QtDeadlineScheduler",
