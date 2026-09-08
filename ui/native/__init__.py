7b823f96369b9759948b409a98f80655121543e8
Cartaz <prenc.francesco@gmail.com>
2026-09-08 20:24:53 +0200

    feat: export Qt deadline scheduler

--- ui/native/__init__.py
+++ ui/native/__init__.py
@@ -1,5 +1,10 @@
 """Native Qt integrations owned by Pi_UI."""
 
 from .agent_process import QProcessAgentTransport, QProcessTransportError
+from .deadline_scheduler import QtDeadlineScheduler
 
-__all__ = ["QProcessAgentTransport", "QProcessTransportError"]
+__all__ = [
+    "QProcessAgentTransport",
+    "QProcessTransportError",
+    "QtDeadlineScheduler",
+]
