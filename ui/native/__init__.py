6013beb43b899e2746f31f47eac5d4526c9a0572
Cartaz <prenc.francesco@gmail.com>
2026-09-08 18:51:33 +0200

    feat(ui): initialize native process integration

---
+++ ui/native/__init__.py
@@ -0,0 +1,5 @@
+"""Native Qt integrations owned by Pi_UI."""
+
+from .agent_process import QProcessAgentTransport, QProcessTransportError
+
+__all__ = ["QProcessAgentTransport", "QProcessTransportError"]
