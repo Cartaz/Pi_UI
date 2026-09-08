518edf0b4264194ef64fa3c983622b4cce5f99a6
Cartaz <prenc.francesco@gmail.com>
2026-09-08 21:01:54 +0200

    feat(ui): export sandbox gate adapter

--- ui/adapters/__init__.py
+++ ui/adapters/__init__.py
@@ -2,5 +2,6 @@
 
 from .agent_adapter import AgentAdapter
 from .preflight_adapter import PreflightAdapter
+from .sandbox_gate_adapter import SandboxGateAdapter
 
-__all__ = ["AgentAdapter", "PreflightAdapter"]
+__all__ = ["AgentAdapter", "PreflightAdapter", "SandboxGateAdapter"]
