74295caec8885cfe0b49981e0c5eeef9a9c27fd2
Cartaz <prenc.francesco@gmail.com>
2026-09-10 12:46:38 +0200

    Export workspace browser adapter

--- ui/adapters/__init__.py
+++ ui/adapters/__init__.py
@@ -3,5 +3,11 @@
 from .agent_adapter import AgentAdapter
 from .preflight_adapter import PreflightAdapter
 from .sandbox_gate_adapter import SandboxGateAdapter
+from .workspace_browser_adapter import WorkspaceBrowserAdapter
 
-__all__ = ["AgentAdapter", "PreflightAdapter", "SandboxGateAdapter"]
+__all__ = [
+    "AgentAdapter",
+    "PreflightAdapter",
+    "SandboxGateAdapter",
+    "WorkspaceBrowserAdapter",
+]
