bc893fd89221fc4df773c74072d0875b733a3034
Cartaz <prenc.francesco@gmail.com>
2026-09-10 12:46:45 +0200

    Export workspace scan runner

--- ui/native/__init__.py
+++ ui/native/__init__.py
@@ -5,12 +5,14 @@ from .app_shutdown import AppShutdownCoordinator
 from .deadline_scheduler import QtDeadlineScheduler
 from .host_runtime import collect_host_runtime_facts
 from .probe_runner import QtProbeRunner
+from .workspace_scan_runner import QtDirectoryScanRunner
 
 __all__ = [
     "AppShutdownCoordinator",
     "QProcessAgentTransport",
     "QProcessTransportError",
     "QtDeadlineScheduler",
+    "QtDirectoryScanRunner",
     "QtProbeRunner",
     "collect_host_runtime_facts",
 ]
